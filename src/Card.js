21d6ec8b5b1411a00a531f75910022eae97b1e96
Dannyobito <dannyobito05@gmail.com>
2022-07-19 13:19:23 +0100

    just some optimizations

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({name,email,id}) => {
+    return (
+        
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt = 'robots'  src = {`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <h2>{email}</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Index.css'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
-
+import 'tachyons'
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
